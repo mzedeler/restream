3183aeba37ce0ac80ff32de7aaa9ec9a9d21abf5
Michael Zedeler <michael@zedeler.dk>
2017-12-19 19:07:31 +0100

    Now rewritten to use sagas.

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
-const { actions, actionTypes, sagas } = require('.');
+const { actions, sagas } = require('.');
 const createSagaMiddleware = require('redux-saga').default;
 const {
+  all,
   fork,
   takeEvery,
   take,
@@ -10,50 +11,62 @@ const fs = require('fs');
 const zlib = require('zlib');
 const configureStore = require('redux-mock-store').default;
 
+const demo = Boolean(process.env.DEMO);
+
 describe('replug', () => {
   it('can provide piping of streams', (done) => {
-
     function* mySagas() {
-      console.log('running mySagas');
       yield fork(sagas);
-      yield takeEvery(
-        actionTypes.READABLE_REGISTER,
-        function* debug({ id }) {
-          yield console.log(`${id}!`);
-        }
-      );
-      yield takeEvery(
-        '*',
-        function* debug({ type, id }) {
-          yield console.log(`${type} ${id}`);
-        }
-      );
-
-      const input = fs.createReadStream(__filename);
-      input.on('data', () => console.log('end'));
-      yield put(actions.readableRegister('input', input));
-
-      yield put(actions.duplexRegister('gzip', zlib.createGzip()));
-
-      const output = fs.createWriteStream(`/tmp/replug-test-output.gz`);
-      yield put(actions.writableRegister('output', output));
-
-      yield put(actions.pipe('input', 'gzip'));
-      yield put(actions.pipe('gzip', 'output'));
-
-      while (true) {
-        const { id } = yield take(actionTypes.WRITABLE_CLOSE);
-        id === 'output' && done();
+      if (demo) {
+        yield takeEvery(
+          '*',
+          function* debug({ type, id }) {
+            // eslint-disable-next-line no-console
+            yield console.log(`${type} ${id}`);
+          }
+        );
       }
-    }
 
-    console.log(1);
+      // Setting up input stream
+      const input = fs.createReadStream(__filename);
+      yield put(actions.readableRegister('input', input));
+      expect(yield take('*')).toMatchSnapshot();
+
+      // Setting up transform stream
+      const gzip = zlib.createGzip();
+      yield put(actions.duplexRegister('gzip', gzip));
+      expect(yield take('*')).toMatchSnapshot();
+
+      // Setting up writable stream (sink)
+      const output = fs.createWriteStream('/tmp/replug-test-output.gz');
+      yield put(actions.writableRegister('output', output));
+      expect(yield take('*')).toMatchSnapshot();
+
+      // Piping input to transform stream
+      yield put(actions.pipe('input', 'gzip'));
+      expect(yield take('*')).toMatchSnapshot();
+      expect(yield take('*')).toMatchSnapshot();
+
+      // Piping transform stream to output
+      yield put(actions.pipe('gzip', 'output'));
+      expect(yield all({
+        outputPipe: yield take('*'),
+        pipeDone: yield take('*'),
+        inputEnd: yield take('*'),
+        gzipFinish: yield take('*'),
+        gzipUnpipe: yield take('*'),
+        gzipEnd: yield take('*'),
+        outputFinish: yield take('*'),
+        outputUnpipe: yield take('*'),
+        outputClose: yield take('*'),
+      })).toMatchSnapshot();
+
+      done();
+    }
 
     const sagaMiddleware = createSagaMiddleware();
     const mockStore = configureStore([sagaMiddleware]);
-    console.log(2);
-    const store = mockStore({});
+    mockStore({});
     sagaMiddleware.run(mySagas);
-    console.log(store);
   });
 });
